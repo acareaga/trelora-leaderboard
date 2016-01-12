b5c9aca10dd01f31ef88ee59cb46017ae8940da2
acareaga <aaroncareaga@gmail.com>
2016-01-12 14:56:10 -0700

    Reverted ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -6,6 +6,5 @@ class RatingsController < ApplicationController
 
   def show
     @rating = Rating.show(params[:id])
-    @transaction = Transaction.show(params[:id]) #<--- poro validate this is the correct transaction
   end
 end
