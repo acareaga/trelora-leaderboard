333b47f7a3eb1c8d7de4f25c571cb62b6fc3c0cd
acareaga <aaroncareaga@gmail.com>
2016-01-11 17:44:16 -0700

    Added endpoints for remaining data

--- app/services/trelora_service.rb
+++ app/services/trelora_service.rb
@@ -8,6 +8,26 @@ class TreloraService
     parse(connection.get("/ratings?api_key=#{ENV['api_key']}"))
   end
 
+  def ratings_by_transaction(id)
+    parse(connection.get("/transacts?api_key=#{ENV['api_key']}/#{id}/ratings"))
+  end
+
+  def transaction(id)
+    parse(connection.get("/transacts?api_key=#{ENV['api_key']}/#{id}"))
+  end
+
+  def member(id)
+    parse(connection.get("/members?api_key=#{ENV['api_key']}/#{id}"))
+  end
+
+  def people(id)
+    parse(connection.get("/people?api_key=#{ENV['api_key']}/#{id}"))
+  end
+
+  def leaderboard
+    parse(connection.get("/ratings?api_key=#{ENV['api_key']}/leaderboard"))
+  end
+
   private
   def parse(response)
     JSON.parse(response.body, symbolize_names: true)
