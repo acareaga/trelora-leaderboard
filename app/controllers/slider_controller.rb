71eff4d8f576d23559e2a97c50b3a3b2dda0b336
joshuajhun <joshuajhun@gmail.com>
2016-01-13 20:21:37 -0700

    refactored code and implemented most recent rating.

--- app/controllers/slider_controller.rb
+++ app/controllers/slider_controller.rb
@@ -2,6 +2,6 @@ class SliderController < ApplicationController
   def index
     @ratings = Rating.all
     @leaderboard = Leaderboard.show
-    @rating = Rating.show(1911)
+    @rating = Rating.show(Rating.all.first[:transact_id])
   end
 end
