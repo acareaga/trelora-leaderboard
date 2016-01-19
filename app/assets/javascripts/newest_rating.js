dde48944f006f64541a39b455425a4e507b8251f
acareaga <aaroncareaga@gmail.com>
2016-01-19 12:16:34 -0700

    Updated for heroku

--- app/assets/javascripts/newest_rating.js
+++ app/assets/javascripts/newest_rating.js
@@ -1,44 +1,6 @@
 function renderNewestRating(rating) {
-  var row = rating.slice(0, 1).map(function(transaction) {
-    // debugger;
+  var row = rating.slice(0, 1).map(function(transaction,index) {
     return (
-        //  "<tr>"
-        // +"<td>"
-        // +"<h4 class='ui image header' id='member_photo'>"
-        // +"</h4>"
-        //   +"</td></div>"
-        //
-        //   +"<td>"
-        //   +"<div class='ui statistic'>"
-        //     +"<div class='value'>"
-        //       + "Newest Rating: " + transaction.stars.toFixed(1)
-        //     +"</div>"
-        //     +"<div class='label'>"
-        //       +"Rating"
-        //     +"</div>"
-        //   +"</div><br><br>"
-        // +"</td></tr>"
-        //
-        //
-        // +"<td>"
-        //   +"<div class='content'>"
-        //   +"<h3 id='people'></h3>"
-        //   +"<div class='sub header'>Customer"
-        //   +"</div>"
-        // +"</td>"
-        //
-        // +"<tr>"
-        // +"<td class='six wide'>"
-        //   +"<div class='content'>"
-        //   +"<h3>"+ transaction.comments +"</h3>"
-        //   +"</div>"
-        //   +"<td>"
-        // +"<td class='four wide'>"
-        //   +"<div class='content'>"
-        //   +"<h3>"+ transaction.code +"</h3>"
-        //   +"</div>"
-        // +"</td></tr>"
-
           "<div class='ui grid'>"
           +"<div class='one wide column'><h1><i class='home icon'></i></h1></div>"
           +"<div class='four wide column'><h1>Newest Rating: " + transaction.stars.toFixed(0) + "!</h1></div>"
@@ -60,17 +22,19 @@ function renderNewestRating(rating) {
           +"<div class='two wide column'></div>"
           +"<div class='sixteen wide column'></div>"
           +"<div class='sixteen wide column'><h2>Previous Ratings</h2></div>"
-          +"<div class='two wide column'><h3>_PERSON_</h3></div>"
-          +"<div class='one wide column'></div>"
-          +"<div class='four wide column'><h3>_TIME SINCE MOST RECENT_</h3></div>"
-          +"<div class='six wide column'></div>"
-          +"<div class='three wide column'>_STAR RATING_</div>"
-          +"<div class='sixteen wide column'></div>"
-          +"<div class='two wide column'><h3>_PERSON_</h3></div>"
-          +"<div class='one wide column'></div>"
-          +"<div class='four wide column'><h3>_TIME SINCE MOST RECENT_</h3></div>"
-          +"<div class='six wide column'></div>"
-          +"<div class='three wide column'>_STAR RATING_</div>"
+          +"<div class='six wide column' id='previous_transaction'"+ index +"></div>"
+
+          // +"<div class='two wide column'><h3>_PERSON_</h3></div>"
+          // +"<div class='one wide column'></div>"
+          // +"<div class='four wide column'><h3>_TIME SINCE MOST RECENT_</h3></div>"
+          // +"<div class='six wide column'></div>"
+          // +"<div class='three wide column'>_STAR RATING_</div>"
+          // +"<div class='sixteen wide column'></div>"
+          // +"<div class='two wide column'><h3>_PERSON_</h3></div>"
+          // +"<div class='one wide column'></div>"
+          // +"<div class='four wide column'><h3>_TIME SINCE MOST RECENT_</h3></div>"
+          // +"<div class='six wide column'></div>"
+          // +"<div class='three wide column'>_STAR RATING_</div>"
       )
   });
   $("#newest_rating").empty().append(row)
@@ -112,3 +76,30 @@ function renderTransactionCode(code){
 function renderCustomerName(customerName){
   $("#customer_name").append(customerName)
 };
+
+function renderPreviousRatingTransactions(previousTransactionRatings){
+  var row = previousTransactionRatings.map(function(transaction,index) {
+    return (
+      +"<div class='two wide column'><h3>" + transaction.person_id + "</h3></div>"
+      +"<div class='one wide column'></div>"
+      +"<div class='four wide column'><h3>" + transaction.created_at + "</h3></div>"
+      +"<div class='six wide column'></div>"
+      +"<div class='three wide column'>" + transaction.stars +"</div>"
+      +"<div class='sixteen wide column'></div>"
+    )}
+  $("#previous_transaction").append(row[0])
+};
+
+// function renderPreviousRatingCustomerName(nameCollection){
+//   var name =  nameCollection.map(function(personName, index){
+//     return(
+//       "<h3 id='people"+index+"'>"+personName.name+"</h3>"
+//     )
+//     })
+//
+//     $("#people0").append(name[0])
+//     $("#people1").append(name[1])
+//     $("#people2").append(name[2])
+//     $("#people3").append(name[3])
+//     $("#people4").append(name[4])
+// };
