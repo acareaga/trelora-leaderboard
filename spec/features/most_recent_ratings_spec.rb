8ff7c673072352ec1dd7335bd8790d262b4b2b26
acareaga <aaroncareaga@gmail.com>
2016-01-20 20:47:12 -0700

    IDs added for capybara feature tests

--- spec/features/most_recent_ratings_spec.rb
+++ spec/features/most_recent_ratings_spec.rb
@@ -1,14 +1,21 @@
-# require 'spec_helper'
-#
-# feature 'User visits the most recent ratings slide' do
-#   scenario 'they see the five most recent customer ratings on the page', :js => true do
-#     visit root_path
-#
-#     sleep(32.second)
-#     expect(page).to have_css '#most_recent_ratings', 'Most Recent Ratings'
-#     expect(page).to have_content('Most Recent Ratings')
-#     expect(page).to have_content('pricing_appointment')
-#     expect(page).to have_content('5.0 RATING')
-#     expect(page).to have_content('Customer')
-#   end
-# end
+require 'spec_helper'
+
+feature 'User visits the most recent ratings slide' do
+  scenario 'they see the five most recent customer ratings on the page', :js => true do
+    visit root_path
+    sleep(32.second)
+
+    expect(page).to have_content('Most Recent Ratings')
+
+    within(:css, "#most_recent_ratings") do
+      expect(page).to have_css '#most_recent_customer_name'
+      expect(page).to have_css '#most_recent_feedback_code'
+      expect(page).to have_css '#most_recent_feedback_comments'
+      expect(page).to have_css '#most_recent_feedback_rating'
+      expect(page).to have_css '#most_recent_member_photo'
+      expect(page).to have_content('pricing_appointment')
+      expect(page).to have_content('5.0 RATING')
+      expect(page).to have_content('Customer')
+    end
+  end
+end
